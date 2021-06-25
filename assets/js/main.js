9b4479386a9a239398c3f61f3263f5f5cb76b8f9
ganeshnaykawadi <ganeshnaykawadi@gmail.com>
2021-06-25 20:22:05 +0530

    functionality of showing random image based on random array index added.

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,15 +11,26 @@ function isOnline() {
   }
 }
 
+//get random array of indexes
+function getIndex(size) {
+
+  let arr = [];
+  while(arr.length < size){
+    var r = Math.floor(Math.random() * size);
+    if(arr.indexOf(r) === -1) arr.push(r);
+  }
+  return arr;
+}
+
 //get images from unplash api
 function getImages() {
-
-  let likes = ['India','Maharashtra', 'Pune', 'Banglore', 'Delhi', 'France', 'Europe', 'Russia', 'UK', 'Canada', 'Australia', 'korea', 'Africa', 'Bhutan', 'Asia', 'Italy', 'Thailand', 'Japan', 'Croatia', 'Israel', 'Philippines'];
+  debugger;
+  let likes = ['India','Maharashtra', 'Pune', 'Kashmir', 'Delhi', 'France', 'Europe', 'Russia', 'Birds', 'Canada', 'Australia', 'korea', 'Africa', 'Bhutan', 'Asia', 'Italy', 'Thailand', 'Japan', 'Croatia', 'Israel', 'Flowers', 'Nature', 'Mumbai', 'Animals'];
   let container = document.getElementById('imageBag');
+  let randomIndex = getIndex(likes.length);
 
-  for(let i = 0; i < likes.length; i++) {
-    container.innerHTML = container.innerHTML +'<div class=\"imgContainer\"><img src=\"https://source.unsplash.com/random/1300x1200/?'+likes[i] +'\" class=\"gallery\" alt=\"'+ likes[i]+'\"><div class=\"overlay\"> Beautiful '+ likes[i] +'</div></div>';
+  for(let i = 0, j = 0; i < likes.length; i++) {
+    j = randomIndex[i];
+    container.innerHTML = container.innerHTML +'<div class=\"imgContainer\"><img src=\"https://source.unsplash.com/random/1300x1200/?'+likes[j] +'\" class=\"gallery\" alt=\"'+ likes[j]+'\"><div class=\"overlay\"> Beautiful '+ likes[j] +'</div></div>';
   }
-
-  console.log(container.innerHTML);
 }
